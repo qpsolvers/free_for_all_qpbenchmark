5956fca6649b09a67fbf0e6c52918a76a88ffb55
Stéphane Caron <stephane.caron@normalesup.org>
2023-12-14 13:33:50 +0100

    Add second type of GNAR problems

--- generate/gnar.py
+++ generate/gnar.py
@@ -18,7 +18,7 @@ import numpy as np
 from qpbenchmark.problem import Problem
 
 
-def get_problem(name: str, alpha: float):
+def get_problem_1(name: str, alpha: float):
     """Get problem instance.
 
     Args:
@@ -37,12 +37,36 @@ def get_problem(name: str, alpha: float):
     )
 
 
-if __name__ == "__main__":
+def get_problem_2(alpha: float):
+    """Get problem instance.
+
+    Args:
+        alpha: Inverse condition number of the problem.
+    """
+    return Problem(
+        P=np.eye(2),
+        q=np.zeros(2),
+        G=None,
+        h=None,
+        A=np.array([[1.0, 0.0], [1.0, alpha]]),
+        b=np.array([0.0, 1.0]),
+        lb=None,
+        ub=None,
+        name=f"GHFFA02_{alpha=}",
+    )
+
+
+def save_problem(problem: Problem) -> None:
     script_dir = path.dirname(path.abspath(__file__))
     data_dir = path.realpath(path.join(script_dir, "../data"))
+    save_path = f"{data_dir}/{problem.name}.npz"
+    print(f"Writing {problem.name} problem to {save_path}...")
+    problem.save(save_path)
+
+
+if __name__ == "__main__":
     for i, alpha in enumerate([1e-2, 1e-4, 1e-6, 1e-8, 1e-10]):
-        name = f"GNAR{i}"
-        problem = get_problem(name, alpha)
-        save_path = f"{data_dir}/{name}.npz"
-        print(f"Writing {name} problem to {save_path}...")
-        problem.save(save_path)
+        problem_1 = get_problem_1(f"GNAR1{i}", alpha)
+        problem_2 = get_problem_2(f"GNAR2{i}", alpha)
+        save_problem(problem_1)
+        save_problem(problem_2)
